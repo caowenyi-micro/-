a2dc2021c50013f4a0d2309f66477a9cf3b1535b
曹文艺 <wenyi.cao@yitu-inc.com>
2021-02-06 11:30:26 +0800

    netty源码分析

---
+++ netty_code/client/HeartBeatReqHandler.java
@@ -0,0 +1,75 @@
+package com.springcloud.platform.web.mybatis.netty.client;
+
+import com.springcloud.platform.web.mybatis.netty.constant.MessageType;
+import com.springcloud.platform.web.mybatis.netty.message.Header;
+import com.springcloud.platform.web.mybatis.netty.message.NettyMessage;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public class HeartBeatReqHandler extends ChannelInboundHandlerAdapter {
+
+    private volatile ScheduledFuture<?> heartBeat;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        super.channelRead(ctx, msg);
+        NettyMessage message = (NettyMessage) msg;
+        // 握手成功，主动发送心跳消息
+        if (message.getHeader() != null && message.getHeader().getType() == MessageType.LOGIN_RESP.value()) {
+            heartBeat = ctx.executor().scheduleAtFixedRate(new HeartBeatReqHandler.HeartBeatTask(ctx), 5, 5, TimeUnit.SECONDS);
+        } else if (message.getHeader() != null && message.getHeader().getType() == MessageType.HEARTBEAT_RESP.value()) {
+            System.out.println("Client receive server heart beat message : ---> " + message);
+        } else {
+            ctx.fireChannelRead(msg);
+        }
+    }
+
+    public class HeartBeatTask implements Runnable {
+
+        private final ChannelHandlerContext ctx;
+
+        public HeartBeatTask(final ChannelHandlerContext ctx) {
+            this.ctx = ctx;
+        }
+
+        @Override
+        public void run() {
+            NettyMessage heartBeat = buildHeartBeat();
+            System.out.println("Client send heart beat message to server : ---> " + heartBeat);
+            ctx.writeAndFlush(heartBeat);
+        }
+
+        private NettyMessage buildHeartBeat() {
+            NettyMessage message = new NettyMessage();
+            Header       header  = new Header();
+            header.setType(MessageType.HEARTBEAT_REQ.value());
+            header.setSessionID(new Date().getTime());
+            header.setLength(20);
+            HashMap<String, Object> attachment = new HashMap<>();
+            attachment.putIfAbsent("name", UUID.randomUUID().toString());
+            header.setAttachment(attachment);
+            header.setPriority((byte) 0);
+            message.setHeader(header);
+            message.setBody(attachment);
+            return message;
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+            throws Exception {
+        cause.printStackTrace();
+        if (heartBeat != null) {
+            heartBeat.cancel(true);
+            heartBeat = null;
+        }
+        ctx.fireExceptionCaught(cause);
+    }
+
+}
